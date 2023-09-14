e16ae0c09dd800736f823af9475dcaa5971d5b97
benwang <ben2820@gmail.com>
2023-09-14 09:36:08 +0800

    compare dist

--- selectData.py
+++ selectData.py
@@ -1,6 +1,7 @@
 import numpy
 import pandas as pd
 import math 
+import numpy as np
 
 def getSimilarData(inputData):
     if inputData['type'] == 'apartment':
@@ -11,3 +12,15 @@ def getSimilarData(inputData):
         data = pd.read_csv('./final_inference/lgbm_透天厝.csv')
         
     # InputData will contain a Chinese address, need to be convert to (lat, long) either TWD97 or WGS84
+    inputLoc = [0, 0]
+    
+    dist = []
+    for idx,item in data.iterrows():
+        tmpLoc = [float(item['x座標']),float(item['y座標'])]
+        tmpDist = math.dist(tmpLoc, inputLoc)
+        dist.append(tmpDist)
+    dist = np.array(dist)
+    idxDist = np.argpartition(dist,20)
+    
+def selectByDist():
+    pass
