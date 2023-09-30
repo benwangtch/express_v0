b6d6846e52ae4730b2d346dead2605a326b6246b
benwang <ben2820@gmail.com>
2023-09-30 17:05:57 +0800

    directory

--- README.md
+++ README.md
@@ -1,5 +1,29 @@
-# express
+# Express
 A house price prediction framework.
+
+## Directory Structure
+
+├── app.py
+├── convertCoord.py
+├── imputeMissingValues.py
+├── inference.py
+├── selectProperties.py
+├── utils.py
+└── model
+    ├── 大樓all.pkl
+    ├── 公寓all.pkl
+    └── 透天厝all.pkl
+└── static
+    └── scripts
+        ├── map.js
+        ├── script.js
+        └── user.js
+    └── styles
+        ├── map.css
+        ├── style.css
+    └── templates
+        └── index.html
+
 
 Run:
 flask --app app run
--- inference.py
+++ inference.py
@@ -20,7 +20,7 @@ def inference(type,inferenceData, inputData):
                         # num_leaves = 32, 
                         
                         metric = 'mape')
-    gbm = joblib.load(f'./lgbm/{type}all.pkl')
+    gbm = joblib.load(f'./model/{type}all.pkl')
     pred = gbm.predict(inferenceData)
     
     output = {'type':inputData['type'],'addr':inputData['addr'],'x座標':inferenceData['x座標'][0],'y座標':inferenceData['y座標'][0],'far':inferenceData['far'][0], 'house_age':inferenceData['house_age'][0], '土地移轉總面積(坪)':inferenceData['土地移轉總面積(坪)'][0], '建物移轉總面積(坪)':inferenceData['建物移轉總面積(坪)'][0],'population_density':inferenceData['population_density'][0], '主建物面積':inferenceData['主建物面積'][0], 'n_c_1000':inferenceData['n_c_1000'][0],'車位移轉總面積(坪)':inferenceData['車位移轉總面積(坪)'][0],'total_floor':inferenceData['total_floor'][0],'lat':inputData['lat'],'lon':inputData['lon'],'price_pin':pred[0] }
